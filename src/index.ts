885f22c2588a439b3f826b4e1e2e3f60229bfab6
Oleg Putseiko <oleg.putseiko@gmail.com>
2025-11-07 19:35:58 +1000

    test(compare): add compare function test cases

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 type Primitive = number | bigint | string | symbol | boolean | null | undefined;
 type AnyObject = Record<string | symbol | number, any>;
 
-type PreferredType = 'string' | 'number';
-type Hint = 'string' | 'number' | 'default';
-
 type Predicate<TA = unknown, TB = TA> = (a: TA, b: TB) => number;
 type Comparator = <TA = unknown, TB = TA>(
   a: TA,
@@ -12,26 +9,13 @@ type Comparator = <TA = unknown, TB = TA>(
 ) => number;
 
 const STRING_PRIMITIVE_METHOD_NAMES = ['toString', 'valueOf'];
-const NON_STRING_PRIMITIVE_METHOD_NAMES = ['valueOf', 'toString'];
 
-const isString = (value: unknown): value is string => typeof value === 'string';
-const isBigint = (value: unknown): value is bigint => typeof value === 'bigint';
 const isObject = (value: unknown): value is AnyObject =>
   typeof value === 'object' && value !== null;
 
-const isBigintString = (value: string) => /^\d+(\.\d+)?n?$/u.test(value);
-
 /** @ref ECMA-262 */
-const stringToBigint = (value: string) => BigInt(value.replace(/n$/, ''));
-
-/** @ref ECMA-262 */
-const ordinaryToPrimitive = (value: AnyObject, hint: Hint): Primitive => {
-  const methodNames =
-    hint === 'string'
-      ? STRING_PRIMITIVE_METHOD_NAMES
-      : NON_STRING_PRIMITIVE_METHOD_NAMES;
-
-  for (const name of methodNames) {
+const ordinaryToString = (value: AnyObject): Primitive => {
+  for (const name of STRING_PRIMITIVE_METHOD_NAMES) {
     if (typeof value[name] === 'function') {
       const primitive = value[name]();
 
@@ -40,23 +24,6 @@ const ordinaryToPrimitive = (value: AnyObject, hint: Hint): Primitive => {
   }
 
   throw new TypeError('Cannot convert object to primitive value');
-};
-
-/** @ref ECMA-262 */
-const toPrimitive = (
-  value: unknown,
-  preferredType?: PreferredType,
-): Primitive => {
-  if (!isObject(value)) return value as Primitive;
-
-  if (typeof value[Symbol.toPrimitive] === 'function') {
-    const primitive = value[Symbol.toPrimitive](preferredType ?? 'default');
-
-    if (!isObject(primitive)) return primitive;
-    throw new TypeError('Cannot convert object to primitive value');
-  }
-
-  return ordinaryToPrimitive(value, preferredType ?? 'number');
 };
 
 /** @ref ECMA-262 */
@@ -69,9 +36,23 @@ const toString = (value: unknown): string => {
     case 'undefined':
       return String(value);
 
-    case 'object':
+    case 'object': {
       if (value === null) return String(value);
-      return toString(toPrimitive(value, 'string'));
+
+      const toPrimitive =
+        Symbol.toPrimitive in value ? value[Symbol.toPrimitive] : undefined;
+
+      const primitive: Primitive =
+        typeof toPrimitive === 'function'
+          ? toPrimitive('string')
+          : ordinaryToString(value);
+
+      if (isObject(primitive)) {
+        throw new TypeError('Cannot convert object to primitive value');
+      }
+
+      return toString(primitive);
+    }
 
     default:
       throw new TypeError(`Cannot convert ${typeof value} to string value`);
@@ -79,61 +60,18 @@ const toString = (value: unknown): string => {
 };
 
 /** @ref ECMA-262 */
-const toNumeric = (value: unknown): number | bigint => {
-  const primitive = toPrimitive(value, 'number');
+const isLessThan = (a: string, b: string): boolean => {
+  const length = Math.min(a.length, b.length);
 
-  return isBigint(primitive) ? primitive : Number(primitive);
-};
+  for (let i = 0; i < length; i++) {
+    const aChar = a.charCodeAt(i);
+    const bChar = b.charCodeAt(i);
 
-/** @ref ECMA-262 */
-const isLessThan = (
-  a: unknown,
-  b: unknown,
-  isLeftFirst: boolean = true,
-): boolean | undefined => {
-  let aPrimitive: Primitive;
-  let bPrimitive: Primitive;
-
-  if (isLeftFirst) {
-    aPrimitive = toPrimitive(a);
-    bPrimitive = toPrimitive(b);
-  } else {
-    bPrimitive = toPrimitive(b);
-    aPrimitive = toPrimitive(a);
+    if (aChar < bChar) return true;
+    if (aChar > bChar) return false;
   }
 
-  if (isString(aPrimitive) && isString(bPrimitive)) {
-    const length = Math.min(aPrimitive.length, bPrimitive.length);
-
-    for (let i = 0; i < length; i++) {
-      const aChar = aPrimitive.charCodeAt(i);
-      const bChar = bPrimitive.charCodeAt(i);
-
-      if (aChar < bChar) return true;
-      if (aChar > bChar) return false;
-    }
-
-    return aPrimitive.length < bPrimitive.length;
-  }
-
-  if (isBigint(aPrimitive) && isString(bPrimitive)) {
-    if (!isBigintString(bPrimitive)) return undefined;
-
-    return aPrimitive < stringToBigint(bPrimitive);
-  }
-
-  if (isString(aPrimitive) && isBigint(bPrimitive)) {
-    if (!isBigintString(aPrimitive)) return undefined;
-
-    return stringToBigint(aPrimitive) < bPrimitive;
-  }
-
-  const aNumeric = toNumeric(aPrimitive);
-  const bNumeric = toNumeric(bPrimitive);
-
-  if (Number.isNaN(aNumeric) || Number.isNaN(bNumeric)) return undefined;
-
-  return aNumeric < bNumeric;
+  return a.length < b.length;
 };
 
 /**
